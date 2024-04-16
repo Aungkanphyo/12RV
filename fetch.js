85dd128b5c46f55d6d487b5fabdd0d97348bc3fd
aungkanphyoo <aungkanphyo1@gmail.com>
2024-04-16 15:04:16 +0630

    async & await

--- fetch.js
+++ fetch.js
@@ -1,11 +1,18 @@
-fetch("Todos/hlahla.json")
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => console.log(res));
+// fetch("Todos/hlahla.json")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => console.log(res));
 
-const doSomething = () => {
-  return fetch("Todos/hlahla.json");
+// const doSomething = () => {
+//   return fetch("Todos/hlahla.json");
+// };
+// const data = doSomething();
+// console.log(data);
+
+const getTodos = async () => {
+  const todos = await fetch("Todos/hlahla.json");
+  const data = await todos.json();
+  console.log(data);
 };
-const data = doSomething();
-console.log(data);
+getTodos();
